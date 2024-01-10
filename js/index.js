c33be426f5d44c17f7c14c3ecc00613e0e5417ea
Liborio Daniele Frasca <frascaliboriodaniele@gmail.com>
2024-01-10 14:39:22 +0100

    mod

--- js/index.js
+++ js/index.js
@@ -17,35 +17,6 @@ window.onscroll = function () {
   }
 };
 
-//Funzione che cambia classe alla foto me per allagarla
-setTimeout(function () {
-  document.getElementById("img").classList.add("opacity");
-}, 0.1);
-
-// Funzione per controllare quando gli elementi sono visibili nella finestra di visualizzazione
-function isElementInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
-// Funzione per mostrare gli article del portfolio allo scroll
-function showVisibleElements() {
-  var portfolioItems = document.querySelectorAll(".portfolio-item");
-  portfolioItems.forEach(function (item) {
-    if (isElementInViewport(item)) {
-      item.classList.add("visible");
-    }
-  });
-}
-window.addEventListener("scroll", showVisibleElements);
-document.addEventListener("DOMContentLoaded", showVisibleElements);
-
 // Alert all'invio del form
 const contForm = document.getElementById("contactForm");
 contForm.addEventListener("submit", function (event) {
